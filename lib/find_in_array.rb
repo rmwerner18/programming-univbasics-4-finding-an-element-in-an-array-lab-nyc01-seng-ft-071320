27d74d43038eeb924663182f803698f212a3599a
Ryan Werner <rmwerner@umich.edu>
2020-06-15 17:34:58 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,8 @@
 def find_element_index(array, value_to_find)
   count = 0
-  while array[count] != value_to_find do
+  while array[count] do
     count += 1
+    break if array[count] == value_to_find
+    return count
   end
 end
